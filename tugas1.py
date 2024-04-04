dce640c814d94d8b1e08570f1f36f6fc9c3537dc
Ricovadt2348 <muhammad.ridho123@icloud.com>
2024-04-04 13:04:21 +0800

    commit eurico arva adytia

--- tugas1.py
+++ tugas1.py
@@ -13,3 +13,19 @@ print('agama : ',agama)
 print('alamat : ',alamat)
 
 print("================================================")
+
+nama = 'eurico arva adytia'
+kelas = 'X RPL 2'
+absen = '11'
+umur = '15'
+agama = 'kristen'
+alamat = 'sweta'
+
+print('nama : ',nama)
+print('kelas : ',kelas)
+print('absen : ',absen)
+print('umur : ',umur)
+print('agama : ',agama)
+print('alamat : ',alamat)
+
+print("================================================")
