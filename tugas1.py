8ef001cd1a6cf78d722bb8163e168ed1863f34aa
Ridho292 <muhammad.ridho123@icloud.com>
2024-04-04 12:47:52 +0800

    commit muhammad ridho

---
+++ tugas1.py
@@ -0,0 +1,15 @@
+nama = 'muhammad ridho'
+kelas = 'X RPL 2'
+absen = '23'
+umur = '16'
+agama = 'islam'
+alamat = 'Getap Barat'
+
+print('nama : ',nama)
+print('kelas : ',kelas)
+print('absen : ',absen)
+print('umur : ',umur)
+print('agama : ',agama)
+print('alamat : ',alamat)
+
+print("================================================")
